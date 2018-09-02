36e7ed306f7c0bcf7568aadfcd3cf46994d97b77
Jonathan Schmold <ImFluffeh@hotmail.ca>
2018-09-02 13:28:06 -0600

    Static functions on user-accounts tested

--- src/user-account.spec.ts
+++ src/user-account.spec.ts
@@ -1,38 +1,306 @@
 import "mocha";
+import { assert } from 'chai';
+import { UserAccount, IUserAccount } from './user-account';
+import { Password, IPassword } from "./password";
+import { SecondaryEmail } from "./secondary-email";
+import { ExpireableToken } from "./expireable-token";
+
+function refCheck(a: any, b: any) {
+  if (typeof a !== "object") return true;
+  for(let key of Object.keys(a)) {
+    if (b[key] == null) continue;
+    if (typeof a[key] === 'object' && refCheck(a[key], b[key]) !== true) return false;
+  }
+  return true;
+}
+
+function immutabilityRespect<T>(fn: () => T[] | Promise<T[]>) {
+  it('respected immutability', async function() {
+    let [ a, b ] = await fn(); 
+    assert.isTrue(refCheck(a, b), 'Did not respect immutability');
+  })
+}
+
+let password: IPassword;
+
+function generateTestAccount(): IUserAccount {
+  return {
+    primaryEmail: 'me@jonathanschmold.ca',
+    password,
+    status: 'active',
+    emails: []
+  }
+}
 
 describe('UserAccount', function() {
+
+  let emails = () => [
+    new SecondaryEmail('one@example.com'),
+    new SecondaryEmail('two@example.com'),
+    new SecondaryEmail('three@example.com'),
+  ]
+
+  this.beforeEach(async () => {
+    password = password || await Password.create('alskjdflkjaklsdjflk');
+  });
   describe('$createActivation', function() {
-    
+    immutabilityRespect(async () => {
+      let acc = await generateTestAccount();
+      return [ acc, UserAccount.createActivation(acc) ];
+    })
+    it('generates a new token with the activation', async function() {
+      let obj = await generateTestAccount();
+      let newObj = UserAccount.createActivation(obj);
+      assert.isDefined(newObj.activation);
+      assert.isString(newObj.activation.token, 'Token is not a string');
+      assert.equal(newObj.primaryEmail, newObj.activation.email, 'Emails did not match');
+      assert.isDefined(newObj.activation.created, 'Did not assign a created date');
+    })
   })
   describe('$activate', function() {
-    
+    immutabilityRespect(async () => {
+      let acc = await generateTestAccount();
+          acc.activation = new ExpireableToken(acc.primaryEmail);
+      return [ acc, UserAccount.activate(acc, acc.activation.token) ];
+    })
+    it('errors if tokens do not align', async function() {
+      let obj = await generateTestAccount();
+          obj.activation = new ExpireableToken(obj.primaryEmail);
+      
+      assert.throws(() => {
+          UserAccount.activate(obj, 'asldkjflkasjdflkjp9u13kj')    
+        },
+        /token/i, 
+        'Did not throw an error, or did not throw an error about token alignment'
+      );
+    })
+    it('Errors if there is no token to activate', async function() {
+      let user = await generateTestAccount();
+      assert.throws(
+        () => UserAccount.activate(user, '13o4oou4kljlk12j4lkjl1j2k4'),
+        /exist/i, 
+        'Did not throw an error about token not existing'
+      );
+    })
+    it('Returns new object without activation if success', async function() {
+      let user = await generateTestAccount();
+          user.activation = new ExpireableToken(user.primaryEmail);
+      let ret = UserAccount.activate(user, user.activation.token);
+      assert.isTrue(user !== ret, 'Did not respect immutability');
+      assert.isDefined(user.activation, 'Should not have deleted activation off original object');
+      assert.isUndefined(ret.activation, 'Did not delete activation token');
+    })
   })
   describe('$addEmail', function() {
-    
+    immutabilityRespect(async () => {
+      let acc = await generateTestAccount();
+      return [ acc, UserAccount.addEmail(acc, 'atet@exam.com') ];
+    })
+    it('Refuses non-string email arg', async function() {
+      let user = await generateTestAccount();
+      assert.throws(
+        // @ts-ignore
+        () => UserAccount.addEmail(user, 129890801283), 
+        /string/i,
+        'Should not support anything but strings'
+      )
+      
+    })
+    it('Returns a new object with the email inside the emails property', async function() {
+      let user = await generateTestAccount();
+      let email = 'test@example33.com';
+      let ret = UserAccount.addEmail(user, email, 'A random label')
+
+      assert.isTrue(user !== ret, 'Does not respect immutability');
+      assert.isAtLeast(ret.emails.findIndex(em => em.email === email), 0, 'Email was not added');
+    })
   })
   describe('$removeEmail', function() {
-    
+    immutabilityRespect(async () => {
+      let acc = await generateTestAccount();
+          acc.emails = emails();
+      return [ acc, UserAccount.removeEmail(acc, 'one@example.com') ];
+    })
+    it('Does nothing if the email does not exist', async function() {
+      let user = await generateTestAccount();
+          user.emails = emails();
+      let ret = UserAccount.removeEmail(user, 'four@example.com');
+      assert.equal(ret.emails.length, 3, 'Removed an email but should not have')
+    })
+    it('Removes the correct email with a string arg', async function() {
+      let user = await generateTestAccount();
+          user.emails = emails();
+      let ret = UserAccount.removeEmail(user, 'one@example.com');
+
+      assert.equal(ret.emails.findIndex(em => em.email === 'one@example.com'), -1, 'Did not remove the email');
+    })
+    it('Removes the correct email with a number arg', async function() {
+      let user = await generateTestAccount();
+          user.emails = emails();
+      let ret = UserAccount.removeEmail(user, 0);
+
+      assert.equal(ret.emails.findIndex(em => em.email === 'one@example.com'), -1, 'Did not remove the email');
+    })
+    it('Respects immutability with the returned object', async function() {
+      let user = await generateTestAccount();
+          user.emails = emails();
+      let ret = UserAccount.removeEmail(user, 0);
+      assert.isTrue(user !== ret)
+    })
+    it('Respects immutability with the emails array ', async function() {
+      let user = await generateTestAccount();
+          user.emails = emails();
+      let ret = UserAccount.removeEmail(user, 0);
+      assert.isTrue(user.emails !== ret.emails);
+    })
   })
   describe('$createReset', function() {
-    
+
+    it('returns a new object with the reset token', async function() {
+      let user = await generateTestAccount();
+      let ret = UserAccount.createReset(user);
+      assert.isTrue(user !== ret, 'Did not respect immutability');
+      assert.isDefined(ret.reset, 'Reset was not created');
+      assert.isDefined(ret.reset.token, 'Token was not created');
+      assert.equal(ret.reset.email, user.primaryEmail, 'Did not use primary email');
+
+      
+
+    })
+    immutabilityRespect(async () => {
+      let acc = await generateTestAccount();
+      return [ UserAccount.createReset(acc), acc ];
+    })
   })
   describe('$applyReset', function() {
-    
+    immutabilityRespect(async () => {
+      let acc = await generateTestAccount();
+          acc.reset = new ExpireableToken(acc.primaryEmail);
+      return [ UserAccount.applyReset(acc, acc.reset.token), acc ];
+    })
+    it('errors if there is no reset', async function () {
+      let user = await generateTestAccount();
+      assert.throws(
+        () => UserAccount.applyReset(user, 'asdlkjflkjalkdfjlkjasdf'),
+        /exist/i,
+        'Did not throw an error about the token not existing'
+      );
+    })
+    it('errors if the reset is not valid', async function() {
+      let user = await generateTestAccount();
+          user.reset = new ExpireableToken(user.primaryEmail);
+      assert.throws(
+        () => UserAccount.applyReset(user, ';lkjd;ljkasd;ljkasd;jlkasdfjkasd'),
+        /align/i,
+        'Does not throw an error about tokens not aligning'
+      );
+    })
+    it('returns a new object without the reset property', async function() {
+      let user = await generateTestAccount();
+          user.reset = new ExpireableToken(user.primaryEmail);
+      let token = user.reset.token;
+      let ret = UserAccount.applyReset(user, token);
+      assert.isTrue(user !== ret, 'Does not respect immutability');
+    })
   })
   describe('$setPrimaryEmail', function() {
-    
+    immutabilityRespect(async () => {
+      let acc = await generateTestAccount();
+      return [ UserAccount.setPrimaryEmail(acc,'asdklfjlkjaslkdfjkjasd@kjadsfkljkla.com'), acc ];
+    })
+
+    it('returns a new object with the changed primary email', async function() {
+      let user = await generateTestAccount();
+      let currentEmail = user.primaryEmail;
+      let newEmail = 'jonathan@thisistest.com';
+      let ret = UserAccount.setPrimaryEmail(user, newEmail);
+
+      assert.isTrue(ret !== user, 'Did not respect immutability');
+      assert.isTrue(ret.emails !== user.emails, 'Did not respect immutability on emails array')
+      assert.notEqual(ret.primaryEmail, currentEmail, 'Did not modify emails');
+      assert.equal(ret.primaryEmail, newEmail);
+      
+    });
+    it('sets the correct email with a number argument', async function() {
+      let user = await generateTestAccount();
+          user.emails = emails()
+      let currentEmail = user.primaryEmail;
+      let newEmail = user.emails[2].email;
+      let ret = UserAccount.setPrimaryEmail(user, 2);
+
+      assert.isTrue(ret !== user, 'Did not respect immutability');
+      assert.isTrue(ret.emails !== user.emails, 'Did not respect immutability on emails array')
+      assert.isTrue(ret.password !== user.password, 'Did not respect immutability on password object')
+      assert.notEqual(ret.primaryEmail, currentEmail, 'Did not modify emails');
+      assert.equal(ret.primaryEmail, newEmail);
+    })
   })
   describe('$setPassword', function() {
-    
+    immutabilityRespect(async () => {
+      let acc = await generateTestAccount();
+      return [ await UserAccount.setPassword(acc, 'asdkfkjalksdjf'), acc ];
+    })
+    it('returns a new password object', async function() {
+      let user = await generateTestAccount();
+      let pwdString = 'ldksflkjalsdfjlasdf';
+      let ret = await UserAccount.setPassword(user, pwdString);
+
+      assert.isTrue(user !== ret, 'Did not respect immutability on object');
+      assert.isTrue(user.emails !== ret.emails, 'Did not respect immutability on emails');
+      assert.isTrue(user.password !== ret.password, 'Did not respect immutability on password');
+    })
+    it('hashes the password', async function() {
+      let user = await generateTestAccount();
+      let pwdString = 'asdflkjlkadklfjakjsdf';
+      let ret = await UserAccount.setPassword(user, pwdString);
+
+      assert.isTrue(user.password.hash !== ret.password.hash, 'Did not hash password');
+    })
   })
   describe('$forceStatus', function() {
-    
+    immutabilityRespect(async () => {
+      let acc = await generateTestAccount();
+      return [ UserAccount.forceStatus(acc, 'locked'), acc ];
+    })
+    it('returns a new object with the correct status', async function() {
+      let obj = await generateTestAccount();
+      let ret = UserAccount.forceStatus(obj, 'locked');
+      assert.notEqual(obj.status, ret.status, 'Did not assign new status (same as old)');
+      assert.equal(ret.status, 'locked', 'Did not assign new status')
+    })
+    it('errors on invalid status', async function() {
+      let obj = await generateTestAccount();
+
+      assert.throws(
+        // @ts-ignore
+        () => UserAccount.forceStatus(obj, 'lkjalsdfkjad'),
+        /invalid.*status/i,
+        'Did not throw an error about invalid status'
+      );
+    })
   })
   describe('$deactivate', function() {
-    
+    immutabilityRespect(async () => {
+      let acc = generateTestAccount();
+      return [ UserAccount.deactivate(acc), acc ];
+    })
+    it('returns a new object with the locked status', async function() {
+      let obj = await generateTestAccount();
+      let ret = UserAccount.deactivate(obj);
+      assert.equal(ret.status, 'locked', 'Did not set status');
+    })
   })
   describe('$reactivate', function() {
-    
+    immutabilityRespect(async () => {
+      let acc = generateTestAccount();
+          acc.status = 'locked';
+      return [ UserAccount.reactivate(acc), acc ];
+    })
+    it('returns a new object with the locked status', async function() {
+      let obj = await generateTestAccount();
+      let ret = UserAccount.reactivate(obj);
+      assert.equal(ret.status, 'active', 'Did not set status');
+    })
   })
 
 
--- src/user-account.ts
+++ src/user-account.ts
@@ -20,17 +20,17 @@ let statusKeys: AccountStatus[] = [ 'active', 'locked', 'activation' ];
 
 export interface IUserAccount {
   username?: string;
-  password: IPassword;
+  password?: IPassword;
   primaryEmail: string;
   reset?: IExpireableToken;
-  emails?: SecondaryEmail[];
+  emails: SecondaryEmail[];
   status: AccountStatus;
   activation?: ExpireableToken;
 }
 
 export class UserAccount implements IUserAccount {
   reset?: IExpireableToken;
-  emails?: SecondaryEmail[];
+  emails: SecondaryEmail[] = [];
   activation?: ExpireableToken;
 
   /**
@@ -42,6 +42,8 @@ export class UserAccount implements IUserAccount {
 
   /**
    * Set the default status 
+   * @param obj
+   * @throws Invalid status
    */
   static set DefaultStatus(obj: AccountStatus) {
     if (statusKeys.indexOf(obj) === -1)
@@ -51,83 +53,198 @@ export class UserAccount implements IUserAccount {
 
   private static _defaultStatus: AccountStatus  = 'activation';
 
+  /**
+   * Generate an activation token for a user account
+   * @param user 
+   */
   static createActivation(user: IUserAccount): IUserAccount {
-    return { ... user, activation: new ExpireableToken(user.primaryEmail) };
+    let obj = UserAccount.fromObject(user);
+    return { ... obj, activation: new ExpireableToken(user.primaryEmail), emails: Array.from(user.emails || []) };
   }
 
+  /**
+   * Attempt to activate a user account using a token
+   * @param user 
+   * @param token 
+   * @throws No user activation
+   * @throws Token does not match
+   */
   static activate(user: IUserAccount, token: string): IUserAccount {
     if (user.activation == null)
-      throw new Error('Unable to activate object without activation created');
+      throw new Error('Unable to activate object. Activation does not exist on object.');
     
     if (ExpireableToken.validateStrings(user.activation, token) === false)
       throw new Error('Unable to activate. Tokens for activation do not match.');
 
-    let obj = {...user};
+    let cloned = UserAccount.fromObject(user);
+    let obj = {...cloned, emails: Array.from(user.emails || [])};
 
     delete obj.activation;
 
     return obj;
   }
 
+  /**
+   * Add an email to a user account with an optional label
+   * @param user 
+   * @param email 
+   * @param label 
+   */
   static addEmail(user: IUserAccount, email: string, label?: string): IUserAccount {
-    let em = new SecondaryEmail(email, label);
-    let obj = { ... user }
-        obj.emails.push(em);
-    return obj;
+    if (typeof email !== 'string')
+      throw new TypeError('Email needs to be of type string');
+    let emails = [
+      ... (user.emails),
+      new SecondaryEmail(email, label)
+    ];
+    return { ... UserAccount.fromObject(user), emails }
   }
 
+  /**
+   * Remove an email either at a specified index, or by a string.
+   * If the email is not found, no failure is thrown. It returns 
+   * the account object.
+   * @param user 
+   * @param email 
+   */
   static removeEmail(user:IUserAccount, email: string | number): IUserAccount {
     let index = email as number;
     if (typeof email === 'string') {
       index = user.emails.findIndex((val: SecondaryEmail) => val.email === email as string);
     }
-    if (typeof index !== 'number' || index < 0) return user;
 
-    let obj = {... user};
+    if (typeof index !== 'number' || index < 0) return { ... user };
+
+    let obj = {... UserAccount.fromObject(user) , emails: Array.from(user.emails) };
         obj.emails.splice(index, 1);
     return obj;
   }
 
+  /**
+   * Create a reset token on a user account
+   * @param user 
+   */
   static createReset(user: IUserAccount): IUserAccount {
-    let obj = { ... user };
+    let obj = { ... UserAccount.fromObject(user), emails: Array.from(user.emails || []) };
         obj.reset = new ExpireableToken(obj.primaryEmail);
     return obj;
   }
 
+  /**
+   * Attempt to validate a token against an account's password reset
+   * @param user 
+   * @param token 
+   * @throws When token is invalid
+   */
   static applyReset(user: IUserAccount, token: string): IUserAccount {
+    if (user.reset == null)
+      throw new Error('Reset does not exist on object');
+
     if (ExpireableToken.validateStrings(user.reset, token) === false) 
       throw new Error('Token does not align with reset token');
 
-    let obj = { ... user };
+    let obj = { ... UserAccount.fromObject(user), emails: Array.from(user.emails || []) };
     delete obj.reset;
     return obj;
   }
 
+  /**
+   * Either set a new email as the primary email, or set the primary
+   * email to the one in the emails field at index obj
+   * 
+   * This does not add the email to the known emails
+   * @param user 
+   * @param obj 
+   * @throws No email to set
+   */
   static setPrimaryEmail(user: IUserAccount, obj: string | number): IUserAccount {
     let primaryEmail = obj as string;
-    if (typeof obj === 'number') {
+    let emails = Array.from(user.emails);
+
+    if (typeof obj === 'number')
       primaryEmail = user.emails[obj].email;
-    }
+    
 
     if (primaryEmail == null) throw new Error('No email to set');
     
-    return { ... user, primaryEmail };
+    return { ... UserAccount.fromObject(user), emails, primaryEmail };
   }
 
+  /**
+   * Forcefully set a password on a user account
+   * @param user 
+   * @param pwd 
+   */
   static async setPassword(user: IUserAccount, pwd: string | null): Promise<IUserAccount> {
-    return { ... user, password: pwd == null ? null : await Password.create(pwd) };
+    return { 
+      ... UserAccount.fromObject(user),
+      password: pwd == null ? null : await Password.create(pwd),
+    };
   }
 
+  /**
+   * Force a status on a user account
+   * @param user 
+   * @param status 
+   */
   static forceStatus(user: IUserAccount, status: AccountStatus): IUserAccount {
-    return {... user, status };
+    if (statusKeys.indexOf(status) === -1)
+      throw new Error(`Invalid status key ${status}. Valid keys are ${statusKeys.join(', ')}`);
+    return {... UserAccount.fromObject(user), status };
   }
 
+  /**
+   * Set a user account to locked
+   * @param user 
+   */
   static deactivate(user: IUserAccount): IUserAccount {
-    return { ... user, status: 'locked' };
+    return { ... UserAccount.fromObject(user), status: 'locked' };
   }
 
+  /**
+   * Set a user account to active
+   * @param user 
+   */
   static reactivate(user: IUserAccount): IUserAccount {
-    return { ... user, status: 'active' };
+    return { ... UserAccount.fromObject(user), status: 'active' };
+  }
+
+  /**
+   * Creates an IUserAccount, setting only the primaryEmail 
+   * and the password. The account status is derived from 
+   * UserAccount.DefaultStatus.
+   * @param email 
+   * @param password 
+   */
+  static async create(email: string, password?: string): Promise<IUserAccount> {
+    let obj: IUserAccount = {
+      primaryEmail: email,
+      password: null,
+      status: UserAccount.DefaultStatus,
+      emails: []
+    }
+    if (password) obj.password = await Password.create(password)
+    return obj;
+  }
+
+  /**
+   * Create a new UserAccount instance from an IUserAccount.
+   * Also can be used for immutability-respecting clone ops
+   * @param obj 
+   */
+  static fromObject(obj: IUserAccount): UserAccount {
+    let acc = new UserAccount(
+      obj.primaryEmail, 
+      { ... obj.password },
+      obj.status, 
+      obj.username
+    );
+    [ 'activation', "reset" ]
+      .forEach(key => acc[key] = obj[key]);
+    acc.emails = Array.from(obj.emails || [])
+    if (acc.emails.length > 0) acc.emails = acc.emails.map(obj => ({ ... obj }));
+
+    return acc;
   }
 
   /** 
@@ -140,5 +257,119 @@ export class UserAccount implements IUserAccount {
     public username?: string,
   ) { 
     this.emails = []; 
+  }
+
+  /**
+   * Create an activation token on this object
+   */
+  createActivation() {
+    this.activation = new ExpireableToken(this.primaryEmail);
+    return this.activation;
+  }
+
+  /**
+   * Attempt to activate this user account. Will delete activation token.
+   * @param token 
+   */
+  activate(token: string) {
+    if (this.activation == null) 
+      throw new Error('Unable to activate object. Activation token does not exist on object');
+    
+    if (ExpireableToken.validateStrings(this.activation, token) === false)
+      throw new Error('Unable to activate. Tokens for activation do not match')
+    
+    delete this.activation;
+  }
+
+  /**
+   * Add an email to the emails array
+   * @param email 
+   * @param label 
+   */
+  addEmail(email: string, label?: string) {
+    if (typeof email !== 'string')
+      throw new TypeError('Email needs to be of type string');
+    this.emails.push(new SecondaryEmail(email, label));
+  }
+
+  /**
+   * Remove an email from the emails array either via an exact string
+   * or an index (if known)
+   * @param email 
+   */
+  removeEmail(email: string | number) {
+    let index = email as number;
+    if (typeof email === 'string') {
+      index = this.emails.findIndex((val: SecondaryEmail) => val.email === email);
+    }
+    if (typeof index !== 'number' || index < 0) return;
+
+    this.emails.splice(index, 1);
+  }
+
+  /**
+   * Create a new reset token on this user object
+   */
+  createReset() {
+    this.reset = new ExpireableToken(this.primaryEmail);
+  }
+
+  /**
+   * Attempt to validate a token against the reset token on this object
+   * @param token 
+   */
+  applyReset(token: string) {
+    if (ExpireableToken.validateStrings(this.reset, token) === false)
+      throw new Error('Token does not align with the reset token')
+
+    delete this.reset;
+  }
+
+  /**
+   * Set the primary email on this object either via a raw string 
+   * or by looking up one of the stored emails. 
+   * 
+   * This does not add the email to the known emails
+   * @param obj 
+   */
+  setPrimaryEmail(obj: string | number) {
+    if (typeof obj === 'number') {
+      this.primaryEmail = this.emails[obj as number].email;
+      return;
+    }
+    if (typeof obj !== 'string')
+      throw new Error('Invalid type for primary email. String required, got: ' + typeof obj);
+    this.primaryEmail = obj;
+  }
+
+  /**
+   * Force a password on this user object
+   * @param pwd 
+   */
+  async setPassword(pwd: string | null) {
+    this.password = pwd == null ? null : await Password.create(pwd);
+  }
+
+  /**
+   * Force a status on this user object
+   * @param status 
+   */
+  forceStatus(status: AccountStatus) {
+
+    this.status = status;
+  }
+
+  /**
+   * Set this user object's status to locked
+   */
+  deactivate() {
+    this.status = 'locked';
+  }
+
+  /**
+   * Set this user object's status to active
+   */
+  reactivate() {
+    this.status = 'active'
   }
 }
